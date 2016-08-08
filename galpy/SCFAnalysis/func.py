33450b9c2bddee970a1ff134b4d12d9488fce60a
Aladdin Seaifan <aladdin.seaifan@mail.utoronto.ca>
2016-08-08 18:25:42 -0400

    Added a function which returns the forces given Acos

--- galpy/SCFAnalysis/func.py
+++ galpy/SCFAnalysis/func.py
@@ -1,5 +1,10 @@
 import numpy as nu
 
+from Constants import *
+from astropy import units
+from galpy.util.bovy_coords import rect_to_cyl
+from galpy import potential
+
 def meanSquares(raw_dec, fit_dec):
     return (nu.sum((raw_dec - fit_dec)**2)/ len(raw_dec))**.5
 
@@ -10,3 +15,30 @@ def ra_decFit(ra, dec, minRa, maxRa, size=100):
     dec_array = p(ra_array)
     return ra_array, dec_array
 
+
+def getForces(folder):
+    def _getForces(folder):
+        loc = "SCFAnalysis/Orbits/{0}".format(folder)
+        Acos = nu.load(loc + "/Acos.npy")
+        a_nfw = 200./8
+        orbits_pos =nu.load(TIMEORBITS.format(loc, TIME,0.004,1*units.Myr))
+        scf = potential.SCFPotential(Acos=Acos, a=a_nfw, normalize=.35)
+        ps= potential.PowerSphericalPotentialwCutoff(alpha=1.8,rc=1.9/8.,normalize=0.05)
+        mn= potential.MiyamotoNagaiPotential(a=3./8.,b=0.28/8.,normalize=.6)
+        MWPotential= [ps,mn,scf]
+        x,y,z = orbits_pos[-1,:3]
+        R, z, phi = rect_to_cyl(x,y,z)
+        Rforce = potential.evaluateRforces(MWPotential, R,z,phi)
+        zforce = potential.evaluatezforces(MWPotential, R,z,phi)
+        phiforce = potential.evaluatephiforces(MWPotential, R,z,phi)
+        return Rforce, zforce, phiforce
+    if type(folder) is list:
+        forces = nu.empty((len(folder), 3))
+        for i in range(len(folder)):
+            forces[i,:] = _getForces(folder[i])
+        return forces
+    else:
+        return _getForces(folder)
+    
+
+    
